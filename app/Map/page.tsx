23b2bdb609754d5b65dde1a273933b93df43b1de
dyzzie <freya_jj@icloud.com>
2026-02-16 12:33:07 +0000

    changed map url

--- app/Map/page.tsx
+++ app/Map/page.tsx
@@ -8,8 +8,8 @@ export default function MapPage() {
   useEffect(() => {
     const map = L.map("map", {scrollWheelZoom: false}).setView([55.3781, -3.4360],6); // Controls where the map is centred upon load
 
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: "&copy; OpenStreetMap contributors",
+    L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
+     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
     }).addTo(map);
 
     return () => {
@@ -19,4 +19,3 @@ export default function MapPage() {
 
   return <div id="map" className="w-full h-full scroll z-10" />;
 }
-
